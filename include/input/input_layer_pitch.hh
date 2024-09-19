388eb52011904faa67f474f4c4bcb2366fd22a00
Ole Kuhlmann <tim.ole.kuhlmann@cern.ch>
2024-09-19 14:11:13 +0200

    Add value conversion

--- include/input/input_layer_pitch.hh
+++ include/input/input_layer_pitch.hh
@@ -9,19 +9,26 @@ public:
 
     /**
      * @brief Construct a InputLayerPitch object.
-     * @param value_range The range of pitch [mm].
      * @param JSON_name The 'name' field of the custom CCT object (rat::mdl::pathcctcustom).
+     * @param value_range The range of pitch [mm].
      * @param column_name_suffix The suffix to be added to the column name in the output file. Default column name is 'layer_pitch'.
      * 
      * Initialize an InputLayerPitch object to define the parameter range for the pitch of one layer in the CCT.
+     * Values are to be provided in [mm] and will be converted to [m] in the JSON file.
      */
-    InputLayerPitch(std::vector<Json::Value> value_range, std::string JSON_name, std::string column_name_suffix="") {
+    InputLayerPitch(std::string JSON_name, std::vector<Json::Value> value_range, std::string column_name_suffix="") {
         column_name_ = "layer_pitch" + column_name_suffix;
-        range_ = value_range;
 
         JSON_name_ = JSON_name;
         JSON_children_ = {"omega"};
-        JSON_target_ = "scaling"; //unit of field is m
+        JSON_target_ = "scaling"; // unit of field is m
+        
+        range_ = value_range;
+
+        // Convert range to m
+        for (size_t i = 0; i < range_.size(); i++) {
+            range_[i] = range_[i].asDouble() / 1000.0;
+        }
     }
 
 };
