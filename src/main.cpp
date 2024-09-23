5e36b2ed4a4beceb7d226c74120d72c99dadeaaa
Ole Kuhlmann <tim.ole.kuhlmann@cern.ch>
2024-09-23 17:24:40 +0200

    Add example to main

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,48 @@
 #include <string>
+#include <vector>
 #include <constants.h>
+#include "input_param_range_interface.h"
+#include "input_layer_pitch.hh"
+#include "output_criterion_interface.h"
+#include "output_b_multipole.hh"
+#include "output_a_multipole.hh"
+#include "parameter_search.h"
+#include "model_handler.h"
+#include "json_range.hh"
 
 int main()
 {
 
     // Set path to model file
-    std::string model_path = OUTPUT_DIR_PATH + "Sextupole_prototype_ConnectV2_56_5mm_V5";
+    std::string model_path = DATA_DIR_PATH + "Sextupole_prototype_ConnectV2_56_5mm_V5.json";
+    CCTools::ModelHandler modelHandler(model_path);
 
     // Set input parameters
-    // TODO
+    std::vector<std::shared_ptr<InputParamRangeInterface>> inputs;
+
+    InputLayerPitch pitch_outer("custom cct outer", {2.05}, "_outer");
+    InputLayerPitch pitch_inner("custom cct inner", JsonRange::double_linear(2, 2.2, 10000), "_inner");
+
+    inputs.push_back(std::make_shared<InputParamRangeInterface>(pitch_outer));
+    inputs.push_back(std::make_shared<InputParamRangeInterface>(pitch_inner));
 
     // Set output parameters
-    // TODO
+    std::vector<std::shared_ptr<OutputCriterionInterface>> outputs;
+
+    // Add all a_n
+    for (size_t i = 1; i <= 10; i++)
+    {
+        outputs.push_back(std::make_shared<OutputAMultipole>(OutputAMultipole(i)));
+    }
+    // Add all b_n
+    for (size_t i = 1; i <= 10; i++)
+    {
+        outputs.push_back(std::make_shared<OutputBMultipole>(OutputBMultipole(i)));
+    }
 
     // Run grid search
-    // TODO
+    ParameterSearch search(inputs, outputs, modelHandler);
+    search.run();
 
     return 0;
 }
