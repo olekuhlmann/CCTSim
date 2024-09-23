9c94fc9532cb31e2b10f1a3d0a5a518794386dca
Ole Kuhlmann <tim.ole.kuhlmann@cern.ch>
2024-09-23 16:41:57 +0200

    Add test file

--- include/constants.h
+++ include/constants.h
@@ -7,6 +7,8 @@
 
 inline const std::string DATA_DIR_PATH = "../data/";
 inline const std::string OUTPUT_DIR_PATH = "output/";
+inline const std::string TEST_DATA_DIR = "../test_data/";
+
 
 
 #endif // CONSTANTS_H
