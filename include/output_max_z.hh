f26f25135bfb789970d4cba090f49eda560d1ee0
Ole Kuhlmann <tim.ole.kuhlmann@cern.ch>
2024-09-27 16:02:15 +0200

    Add min and max z value outputs

---
+++ include/output_max_z.hh
@@ -0,0 +1,41 @@
+#ifndef OUTPUT_MAX_Z_HH
+#define OUTPUT_MAX_Z_HH
+
+#include "output_criterion_interface.h"
+#include "mesh_data_handler.h"
+#include <json/json.h>
+
+class OutputMaxZ : public OutputCriterionInterface
+{
+public:
+    /**
+     * @brief Construct a new OutputMaxZ object.
+     *
+     * Construct a new OutputMaxZ object. This object will compute maximum z coordinate of the model.
+     */
+    OutputMaxZ()
+    {
+        column_name_ = "z_max";
+        required_calculations_ = {std::type_index(typeid(CCTools::MeshDataHandler))};
+    }
+
+    double computeCriterion(std::vector<std::shared_ptr<CCTools::CalcResultHandlerBase>> calcResults)
+    {
+        // Assert that the passed calculation result handlers are of the correct type
+        if (!checkCalcResultHandlerTypes(calcResults))
+        {
+            throw std::runtime_error("Calculation result handlers of the wrong type have been passed to the max Z criterion.");
+        }
+
+        // Extract the mesh data handler from the calculation result handlers
+        auto mesh_data_handler = std::dynamic_pointer_cast<CCTools::MeshDataHandler>(calcResults[0]);
+
+        // Get the max_z value
+        auto max_z = mesh_data_handler->getMinMaxZValues().second;
+
+        // Return the value
+        return max_z;
+    }
+};
+
+#endif // OUTPUT_MAX_Z_HH
---
+++ include/output_min_z.hh
@@ -0,0 +1,41 @@
+#ifndef OUTPUT_MIN_Z_HH
+#define OUTPUT_MIN_Z_HH
+
+#include "output_criterion_interface.h"
+#include "mesh_data_handler.h"
+#include <json/json.h>
+
+class OutputMinZ : public OutputCriterionInterface
+{
+public:
+    /**
+     * @brief Construct a new OutputMinZ object.
+     *
+     * Construct a new OutputMinZ object. This object will compute minimum z coordinate of the model.
+     */
+    OutputMinZ()
+    {
+        column_name_ = "z_min";
+        required_calculations_ = {std::type_index(typeid(CCTools::MeshDataHandler))};
+    }
+
+    double computeCriterion(std::vector<std::shared_ptr<CCTools::CalcResultHandlerBase>> calcResults)
+    {
+        // Assert that the passed calculation result handlers are of the correct type
+        if (!checkCalcResultHandlerTypes(calcResults))
+        {
+            throw std::runtime_error("Calculation result handlers of the wrong type have been passed to the min Z criterion.");
+        }
+
+        // Extract the mesh data handler from the calculation result handlers
+        auto mesh_data_handler = std::dynamic_pointer_cast<CCTools::MeshDataHandler>(calcResults[0]);
+
+        // Get the min_z value
+        auto min_z = mesh_data_handler->getMinMaxZValues().first;
+
+        // Return the value
+        return min_z;
+    }
+};
+
+#endif // OUTPUT_MIN_Z_HH
